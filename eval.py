2d28e71b19df2b6b61b66ae66169f07aa0d0659f
Mingyi Yan <1800012797@pku.edu.cn>
2024-04-29 04:26:08 +0800

    update eval.py

--- eval.py
+++ eval.py
@@ -15,29 +15,15 @@ from torch.utils.data import DataLoader
 from peft import PeftModel
 import evaluate
 from evaluate import load
+from tqdm import tqdm
+
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--method', type=str, default='bitfit')
     
 args = parser.parse_args()
 
-"""### Load data"""
-
-#dataset = load_dataset("tatsu-lab/alpaca_eval") this line does not work
-#eval_set = load_dataset("tatsu-lab/alpaca_eval", "alpaca_eval")["eval"]
-#eval_set.save_to_disk("./AlpacaEval")
-
-
-eval_set = load_dataset("tatsu-lab/alpaca_eval", "alpaca_eval")["eval"]
-eval_set = eval_set.select(range(500))
-
-eval_set[:3]
-
-"""### Load models"""
-
-#tokenizer = AutoTokenizer.from_pretrained("bigscience/bloom-1b1")
-tokenizer = AutoTokenizer.from_pretrained("bigscience/bloom-1b1")
-base_model = AutoModelForCausalLM.from_pretrained("bigscience/bloom-1b1")
 
 def get_model(method):
     model_path = f"./{method}/chatbot/checkpoint-10000"
@@ -86,7 +72,25 @@ def get_model(method):
         raise ValueError(f"{method} does not exist!")
     return model
 
-ft_model = get_model(args.method)
+
+
+"""### Load data"""
+
+#dataset = load_dataset("tatsu-lab/alpaca_eval") this line does not work
+#eval_set = load_dataset("tatsu-lab/alpaca_eval", "alpaca_eval")["eval"]
+#eval_set.save_to_disk("./AlpacaEval")
+
+
+eval_set = load_dataset("tatsu-lab/alpaca_eval", "alpaca_eval", trust_remote_code=True)["eval"]
+eval_set = eval_set.select(range(500))
+
+
+"""### Load models"""
+
+#tokenizer = AutoTokenizer.from_pretrained("bigscience/bloom-1b1")
+tokenizer = AutoTokenizer.from_pretrained("bigscience/bloom-1b1")
+base_model = AutoModelForCausalLM.from_pretrained("bigscience/bloom-1b1")
+print(f"Load base model successly!")
 
 """### Evaluation"""
 
@@ -102,22 +106,28 @@ def generate_model_output(model, instruction):
         result = tokenizer.decode(otpt, skip_special_tokens=True).replace(input_string, "")
         return result
 
+
 base_model.cuda()
 base_model.eval()
 
 pred_base = []
 
 with torch.no_grad():
-    for data in eval_set:
-        output = generate_model_output(model=base_model,instruction=data['instruction'])
+    for data in tqdm(eval_set, desc='Generating base model outputs'):
+        output = generate_model_output(model=base_model, instruction=data['instruction'])
         pred_base.append(output)
 
+ft_model = get_model(args.method)
+print(f"Load model {args.method} successly!")
+
+base_model.cpu()
+torch.cuda.empty_cache()
 ft_model.cuda()
 ft_model.eval()
 
 pred_ft = []
 with torch.no_grad():
-    for data in eval_set:
+    for data in tqdm(eval_set, desc=f'Generating model {arg.methods} outputs'):
         output = generate_model_output(model=ft_model,instruction=data['instruction'])
         pred_ft.append(output)
 
@@ -157,7 +167,7 @@ Eval_result = {
     "bertscore": sum(ft_bert)/len(ft_bert),
 }
 
-directory = f"./{method}/"
+directory = f"./{args.method}/"
 json_filename = "eval_result.json"
 
 if not os.path.exists(directory):
